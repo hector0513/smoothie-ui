756c470ee6280731d02b9b942d7f99642495394e
hector vasquez <hectorvasquez05@gmail.com>
2020-05-10 18:43:06 -0400

    gql

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  apolloProvider: createProvider(),
+  render: (h) => h(App)
 }).$mount("#app");
 
 console.log(process.env.DATA);
